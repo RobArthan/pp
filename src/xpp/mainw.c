afc132425c3b1580666c24a672ee4d030d048143
Rob Arthan <rda@lemma-one.com>
1993-07-22 15:08:03 +0000

    about to simplify typing stuff. RDA

--- src/xpp/mainw.c
+++ src/xpp/mainw.c
@@ -1,7 +1,7 @@
 
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * $Id$ 
+ * $Id$
  *
  * cmdwin.h -  main window operations for the X/Motif ProofPower
  * Interface
@@ -15,7 +15,7 @@
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 #define _cmdwin
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * include files: 
+ * include files:
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
 #include "xpp.h"
@@ -37,13 +37,12 @@
 
 #include <stdio.h>
 #include<Xm/Text.h>
-#include<Xm/VendorE.h>
+#include<Xm/MainW.h>
 #include<Xm/PanedW.h>
-#include<Xm/ScrolledW.h>
 #include<varargs.h>
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * macro definitions: 
+ * macro definitions:
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
 #define STDIN 0
@@ -52,7 +51,7 @@
 
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * static and external data: 
+ * static and external data:
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
 
@@ -61,34 +60,36 @@ extern char *environ[];
 
 static XtAppContext app;
 
-static Widget toplevel;
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Widget	Parent	Purpose
+ * root		-	the top level of the hierarchy
+ * frame	root	main window
+ * work		frame	work area
+ * display	work	displays application output
+ * command	work	command line input
+ * menubar	frame	the menu bar at the top of the main window
+ * filemenu	menubar	the file menu
+ * editmenu	menubar	the edit menu
+ * cmdmenu	menubar	the command menu
+ * helpmenu	menubar	the help menu
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+static Widget
+	root, frame, work, display, command,
+	menubar, filemenu, editmenu, cmdmenu, helpmenu;
 
-static Widget pp_output_w;
+static int display_cursor = 0;
 
-static Widget pp_input_s_w;
-
-static Widget pp_input_t_w;
-
-static Widget pp_output_s_w;
-
-static Widget pp_output_t_w;
-
-static int wpr_position = 0;
-
-static int in_count = 0;
-
-static char buff[BUFSIZ];
-
-static int cursor = 0;
-
-static int boln_offset = 0;
 
 static char *arglist[10];
 
 static int control_fd;
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * Miscellanea: 
+ * scroll_output: given a buffer, buf, containing ct characters
+ * of interest, write them to the (scrolled text) window w.
+ * If the insertion position of w is visible, then w is scrolled,
+ * otherwise the new characters are written out of sight and
+ * the display in w is left where it is.
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
 void scroll_out(buf, ct, w)
@@ -105,93 +106,260 @@ Widget w;
 
 	if(XmTextPosToXY(w, ins_pos, &dontcare, &dontcare)) {
 		/* insertion position is visible: scroll */
-		XmTextInsert(pp_output_t_w, wpr_position, buf);
+		XmTextInsert(w, display_cursor, buf);
 		ins_pos = XmTextGetInsertionPosition(w);
 		while(!XmTextPosToXY(w, ins_pos, &dontcare, &dontcare)) {
 			XmTextScroll(w, 1);
 			ins_pos = XmTextGetInsertionPosition(w);
 		};
-		wpr_position += ct;
-		XtVaSetValues(pp_output_t_w, XmNcursorPosition, wpr_position, NULL);
-	/*	XmTextShowPosition(pp_output_t_w, wpr_position); */
+		display_cursor += ct;
+		XtVaSetValues(w, XmNcursorPosition, display_cursor, NULL);
+	/*	XmTextShowPosition(w, display_cursor); */
 	} else {
-		XmTextInsert(pp_output_t_w, wpr_position, buf);
-		wpr_position += ct;
+		XmTextInsert(w, display_cursor, buf);
+		display_cursor += ct;
 	}
 }
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * X initialisation: 
+ * X initialisation:
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
 
-setup_windows(argc, argv)
+setup_cmdwin(argc, argv)
 int argc;
 char **argv;
 {
-	Arg args[9];
+	Arg args[12];
+	int i;
+	XmString s1, s2, s3, s4, s5, s6;
 	void monitor_typing();
+	void file_menu_cb(), edit_menu_cb(), cmd_menu_cb(), help_menu_cb();
+
+	diag("cmdwin", "X initialisation");
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Top level and main windows:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+
+	root = XtVaAppInitialize(&app,  "Xpp", NULL, 0,
+		&argc, argv, NULL, NULL);
+
+	frame = XtVaCreateWidget("frame",
+		xmMainWindowWidgetClass,
+		root, NULL);
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Working area:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+
+	work = XtVaCreateWidget("work",
+		xmPanedWindowWidgetClass,
+		frame, NULL);
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Display window:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+
+	i = 0;
+	XtSetArg(args[i], XmNrows,	 		24); ++i;
+	XtSetArg(args[i], XmNcolumns, 			80); ++i;
+	XtSetArg(args[i], XmNeditable, 			False); ++i;
+	XtSetArg(args[i], XmNeditMode,	 		XmMULTI_LINE_EDIT); ++i;
+	XtSetArg(args[i], XmNwordWrap, 			True); ++i;
+	XtSetArg(args[i], XmNscrollHorizontal,		False); ++i;
+	XtSetArg(args[i], XmNscrollVertical, 		True); ++i;
+	XtSetArg(args[i], XmNautoShowCursorPosition, 	False); ++i;
+	XtSetArg(args[i], XmNcursorPositionVisible, 	False); ++i;
+
+	display = XmCreateScrolledText(work, "display", args, i);
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Command window:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+
+	i = 0;
+	XtSetArg(args[i], XmNrows,	 		3); ++i;
+	XtSetArg(args[i], XmNcolumns, 			80); ++i;
+	XtSetArg(args[i], XmNeditable, 			True); ++i;
+	XtSetArg(args[i], XmNeditMode, 			XmMULTI_LINE_EDIT); ++i;
+	XtSetArg(args[i], XmNwordWrap, 			True); ++i;
+	XtSetArg(args[i], XmNscrollHorizontal,		False); ++i;
+	XtSetArg(args[i], XmNscrollVertical, 		True); ++i;
+	XtSetArg(args[i], XmNautoShowCursorPosition, 	True); ++i;
+	XtSetArg(args[i], XmNcursorPositionVisible, 	True); ++i;
 
 
-	pp_output_w = XtCreatePopupShell("output",
-		topLevelShellWidgetClass,
-		toplevel,
-		args, 0);
-		
-	
-	pp_output_s_w = XtVaCreateManagedWidget("pp_output_s_w",
-			xmScrolledWindowWidgetClass, pp_output_w,
-		XmNscrollingPolicy,	XmAPPLICATION_DEFINED,
-		XmNvisualPolicy,	XmVARIABLE,
-		XmNscrollBarDisplayPolicy, XmSTATIC,
-		XmNshadowThickness,	0, NULL);
+	command = XmCreateScrolledText(work, "command", args, i);
 
-	pp_output_t_w = XtVaCreateManagedWidget("pp_output_t_w",
-			xmTextWidgetClass, pp_output_s_w,
-		XmNrows, 	24,
-		XmNcolumns, 	80,
-		XmNeditable, 	False,
-		XmNeditMode, 	XmMULTI_LINE_EDIT,
-		XmNwordWrap, 	True,
-		XmNscrollHorizontal, 	False,
-		XmNscrollVertical, 	True,
-		XmNblinkRate, 	0,
-		XmNautoShowCursorPosition, 	False,
-		XmNcursorPositionVisible, 	False,
-		NULL);
-
-	pp_input_s_w = XtVaCreateManagedWidget("toplevel",
-			xmScrolledWindowWidgetClass, toplevel,
-		XmNscrollingPolicy,	XmAPPLICATION_DEFINED,
-		XmNvisualPolicy,	XmVARIABLE,
-		XmNscrollBarDisplayPolicy, XmSTATIC,
-		XmNshadowThickness,	0, NULL);
-	
-	pp_input_t_w = XtVaCreateManagedWidget("pp_input_t_w",
-			xmTextWidgetClass, pp_input_s_w,
-		XmNrows, 	12,
-		XmNcolumns, 	80,
-		XmNeditable, 	True,
-		XmNeditMode, 	XmMULTI_LINE_EDIT,
-		XmNwordWrap, 	True,
-		XmNscrollHorizontal, 	False,
-		XmNscrollVertical, 	True,
-		XmNblinkRate, 	0,
-		XmNautoShowCursorPosition, 	True,
-		XmNcursorPositionVisible, 	True,
-		NULL);
-
-	XtAddCallback(pp_input_t_w, XmNmodifyVerifyCallback,
+	XtAddCallback(command, XmNmodifyVerifyCallback,
 		monitor_typing, NULL);
 
-	XtRealizeWidget(toplevel);
-	XtPopup(pp_output_w, XtGrabNone);
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Menu bar:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+	s1 = XmStringCreateSimple("File");
+	s2 = XmStringCreateSimple("Edit");
+	s3 = XmStringCreateSimple("Command");
+	s4 = XmStringCreateSimple("Help");
+	menubar = XmVaCreateSimpleMenuBar(frame, "menubar",
+		XmVaCASCADEBUTTON, s1, 'F',
+		XmVaCASCADEBUTTON, s2, 'E',
+		XmVaCASCADEBUTTON, s3, 'C',
+		XmVaCASCADEBUTTON, s4, 'H');
 
+	XmStringFree(s1);
+	XmStringFree(s2);
+	XmStringFree(s3);
+	XmStringFree(s4);
 
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * File menu:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+	s1 = XmStringCreateSimple("New");
+	s2 = XmStringCreateSimple("Save");
+	s3 = XmStringCreateSimple("Quit");
+	s4 = XmStringCreateSimple("Ctrl-C");
+
+	filemenu = XmVaCreateSimplePulldownMenu(
+		menubar, "file_menu", 0, file_menu_cb,
+		XmVaPUSHBUTTON, s1, 'N', NULL, NULL,
+		XmVaPUSHBUTTON, s2, 'S', NULL, NULL,
+		XmVaPUSHBUTTON, s3, 'Q', "Ctrl<Key>c", s4,
+		NULL);
+
+	XmStringFree(s1);
+	XmStringFree(s2);
+	XmStringFree(s3);
+	XmStringFree(s4);
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Edit menu:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+	s1 = XmStringCreateSimple("Cut");
+	s2 = XmStringCreateSimple("Copy");
+	s3 = XmStringCreateSimple("Clear");
+	s4 = XmStringCreateSimple("Paste");
+
+	editmenu = XmVaCreateSimplePulldownMenu(
+		menubar, "edit_menu", 1, edit_menu_cb,
+		XmVaPUSHBUTTON, s1, 'u', NULL, NULL,
+		XmVaPUSHBUTTON, s2, 'o', NULL, NULL,
+		XmVaPUSHBUTTON, s3, 'l', NULL, NULL,
+		XmVaPUSHBUTTON, s4, 'a', NULL, NULL,
+		NULL);
+
+	XmStringFree(s1);
+	XmStringFree(s2);
+	XmStringFree(s3);
+	XmStringFree(s4);
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Command menu:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+	s1 = XmStringCreateSimple("Execute");
+	s2 = XmStringCreateSimple("Ctrl-X");
+
+	cmdmenu = XmVaCreateSimplePulldownMenu(
+		menubar, "command_menu", 2, cmd_menu_cb,
+		XmVaPUSHBUTTON, s1, 'x', "Ctrl<Key>x", s2,
+		NULL);
+
+	XmStringFree(s1);
+	XmStringFree(s2);
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Help menu:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+	s1 = XmStringCreateSimple("General");
+	s2 = XmStringCreateSimple("Other");
+
+	helpmenu = XmVaCreateSimplePulldownMenu(
+		menubar, "help_menu", 3, help_menu_cb,
+		XmVaPUSHBUTTON, s1, 'G', NULL, NULL,
+		XmVaPUSHBUTTON, s2, 'O', NULL, NULL,
+		NULL);
+
+	XmStringFree(s1);
+	XmStringFree(s2);
+
+	XtVaSetValues(menubar, XmNmenuHelpWidget, helpmenu, NULL);
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Management and Realisation:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+	XtManageChild(menubar);
+	XtManageChild(command);
+	XtManageChild(display);
+	XtManageChild(work);
+	XtManageChild(frame);
+
+	XtRealizeWidget(root);
 }
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * Keyboard input processing: 
+ * Menu processing:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+void dummy_menu_cb(w, i, cbs, s)
+Widget w;
+int i;
+XmAnyCallbackStruct *cbs;
+char *s;
+{
+	char buf[4];
+	sprintf(buf, "%d", i);
+	diag(s, buf);
+}
+
+void file_menu_cb(w, i, cbs)
+Widget w;
+int i;
+XmAnyCallbackStruct *cbs;
+{
+	dummy_menu_cb(w, i, cbs, "file menu item");
+}
+
+void edit_menu_cb(w, i, cbs)
+Widget w;
+int i;
+XmAnyCallbackStruct *cbs;
+{
+	dummy_menu_cb(w, i, cbs, "edit menu item");
+}
+
+void cmd_menu_cb(w, i, cbs)
+Widget w;
+int i;
+XmAnyCallbackStruct *cbs;
+{
+	char *cmd;
+	int len;
+	void send_to_application();
+	dummy_menu_cb(w, i, cbs, "command menu item");
+	switch(i) {
+	case 0:
+		cmd = XmTextGetString(command);
+		len = strlen(cmd);
+/* **** complain if len = 0? **** */
+		send_to_application(cmd, len);
+		XmTextSetHighlight(command, 0, len, XmHIGHLIGHT_SELECTED);
+		XmTextSetSelection(command, 0, len, CurrentTime);
+		break;
+	default:
+		break;
+	}
+}
+
+void help_menu_cb(w, i, cbs)
+Widget w;
+int i;
+XmAnyCallbackStruct *cbs;
+{
+	dummy_menu_cb(w, i, cbs, "help menu item");
+}
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Keyboard input processing:
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
 void monitor_typing(w, unused, cbs)
@@ -199,6 +367,9 @@ Widget w;
 XtPointer unused;
 XmTextVerifyCallbackStruct *cbs;
 {
+	static int boln_offset = 0;
+	static int cursor = 0;
+	static char buff[BUFSIZ];
 	static char temp[BUFSIZ];
 	int i, j;
 	void send_to_application();
@@ -235,7 +406,7 @@ cursor, boln_offset);
 }
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * Pseudo-terminal initialisation: 
+ * Pseudo-terminal initialisation:
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
 
@@ -308,7 +479,6 @@ gotpty:
 		if((tty_fd = open("/dev/tty", O_RDWR)) >= 0) {
 			ioctl(tty_fd, TIOCNOTTY, 0);
 		}
-		diag("get_pty", "child: about to execve application");
 		execvp(arglist[0], arglist);
 	/* **** error if reach here **** */
 		msg("system error", "could not exec");
@@ -317,7 +487,7 @@ gotpty:
 }
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * Data transfer from application: 
+ * Data transfer from application:
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 void get_from_application(w, unused)
 Widget w;
@@ -326,14 +496,14 @@ XtPointer unused;
 	int ct;
 	static	char buf[257];
 	while((ct = read(control_fd, buf, 256)) > 0) {
-		scroll_out(buf, ct, pp_output_t_w);
+		scroll_out(buf, ct, display);
 		diag("get_from_application", buf);
 	};
 }
 
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * Data transfer to application: 
+ * Data transfer to application:
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
 
@@ -342,7 +512,7 @@ char *buf;
 int siz;
 {
 /*
-	scroll_out(buf, siz, pp_output_t_w);
+	scroll_out(buf, siz, display);
 */
 	if(write(control_fd, buf, siz) != siz) {
 		diag("system error", "write to application failed");
@@ -357,13 +527,10 @@ int argc;
 char *argv[];
 {
 	diag("cmdwin", "starting");
-	toplevel = XtVaAppInitialize(&app,  "Xpp", NULL, 0,
-		&argc, argv, NULL, NULL);
-	diag("cmdwin", "X initialisation");
-	setup_windows(argc, argv);
+	setup_cmdwin(argc, argv);
 	diag("cmdwin", "Pseudo-terminal initialisation");
 	get_pty(argc, argv);
 	diag("cmdwin", "X display start-up");
-	XtRealizeWidget(toplevel);
+	XtRealizeWidget(root);
 	XtAppMainLoop(app);
 }
