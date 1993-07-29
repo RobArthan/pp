1a559159e11c60c223e2b2fc712ea036e9bb4532
Rob Arthan <rda@lemma-one.com>
1993-07-29 16:53:52 +0000

    *** empty log message ***

---
+++ src/xpp/xmisc.c
@@ -0,0 +1,101 @@
+
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * $Id$
+ *
+ * xmisc.c -  miscellaneous X/Motif routines for the X/Motif ProofPower
+ * Interface
+ *
+ * (c) ICL 1993
+ *
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * macros:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+#define _xmisc
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * include files:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+
+
+#include <stdio.h>
+#include <X11/Xatom.h>
+#include <X11/Intrinsic.h>
+#include <X11/Shell.h>
+#include <Xm/Xm.h>
+
+#include<Xm/Text.h>
+#include<varargs.h>
+
+#include "xpp.h"
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * toggle_menu_item_sensitivity: given a menu w toggle the
+ * sensitivity of the i-th item in the menu.
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+void toggle_menu_item_sensitivity(w, i)
+Widget w;
+int i;
+{
+	Widget *btns;
+	int num_btns;
+	Bool sens;
+	diag("toggle_menu_item_sensitivity", "");
+	XtVaGetValues(w, XmNchildren, &btns,
+		XmNnumChildren, &num_btns, NULL);
+	if (0 <= i && i < num_btns) {
+		XtSetSensitive(btns[i], !XtIsSensitive(btns[i]));
+	}
+}
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * check_text_window_limit: helps to manage the text windows
+ * which may have a size limit. Limits less than 1000 are
+ * ignored. If the text needs to be truncated, tries to remove about
+ * 5% of it, and removes a whole number of lines if that can be
+ * done without deleting more than 10%. Expects to be called *after*
+ * the new text has been inserted.
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+void check_text_window_limit(w, max)
+Widget w;
+long max;
+{
+	XmTextPosition siz;
+	long bytes_to_go;
+
+	char *text, *p;
+	char *fmt =
+	"**** Text lost when buffer exceeded %ld bytes ****\n";
+	char *msg =
+	"**** Text lost when buffer exceeded dddddddddddd bytes ****\n";
+
+	if(max < 1000) {
+		return;
+	};
+
+	siz = XmTextGetLastPosition(w);
+
+	if(siz <= max) {
+		return;
+	};
+
+	bytes_to_go = siz / 20;
+
+	text = XmTextGetString(w);
+
+	for(p = text + bytes_to_go; p - text < 2*bytes_to_go; ++p) {
+		if(*p == '\n') {
+			break;
+		}
+	};
+
+	if(*p == '\n') {
+		bytes_to_go = p - text + 1;
+	};
+
+	sprintf(msg, fmt, max);
+
+	XmTextReplace(w, 0, bytes_to_go, msg);
+
+}
