e737ae2cdc89af96d1567cb43a28b4c0cb6a3123
Rob Arthan <rda@lemma-one.com>
1993-08-06 10:46:28 +0000

    First attempt at real command line processing. RDA

--- src/xpp/xpp.c
+++ src/xpp/xpp.c
@@ -5,8 +5,7 @@
 
 #define _xpp
 
-#define CMD_BUF_SIZ 	256
-#define ARG_BUF_SIZ	1024
+#define MAX_ARGS 	256
 
 #define XtNcommandLine	"commandLine"
 #define XtCCommandLine	"CommandLine"
@@ -17,42 +16,127 @@
 
 #include <X11/StringDefs.h>
 #include <stdio.h>
+#include <string.h>
 #include "xpp.h"
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * Global data
+ * Static and global data
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
-char cmd_buf [CMD_BUF_SIZ];
+char *cmd_buf;
 
-char arg_buf [ARG_BUF_SIZ];
-
-char *arglist[CMD_BUF_SIZ];
+char *arglist[MAX_ARGS + 1];
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * Application resources:
+ * Following option table reserves single lower-case letter
+ * options for the command-line. The resource name is irrelevant.
  * **** **** **** **** **** **** **** **** **** **** **** **** */
-typedef struct {
-	char *command_line
-} AppData;
-
-static AppData app_data;
-
-static XtResource resources [] = {
-	{
-		XtNcommandLine,
-		XtCCommandLine,
-		XtRString,
-		sizeof(char *),
-		XtOffsetOf(AppData, command_line),
-		XtRString,
-		"pp"
-	}
-};
-
 static XrmOptionDescRec options [] = {
-	{"-command", "*commandLine",  XrmoptionSepArg, NULL}
+	{"-command", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-a", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-b", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-c", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-d", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-e", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-f", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-g", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-h", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-i", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-j", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-k", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-l", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-m", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-n", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-o", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-p", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-q", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-r", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-s", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-t", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-u", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-v", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-w", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-x", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-y", "*commandLine",  XrmoptionSkipLine, NULL},
+	{"-z", "*commandLine",  XrmoptionSkipLine, NULL}
 };
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Usage line
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+void usage ()
+{
+	msg( "xpp",
+"usage: xpp [standard X Toolkit options -command] command-line");
+};
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Check whether the -command command-line option is there
+ * Returns number of argv items to skip ((1 or 2)
+ * Error message and exit if no command line at all.
+ * Note can omit -command if there are no X options.
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+int check_sep(argc, argv)
+int argc;
+char **argv;
+{
+	int l;
+	if(argc < 2) {
+		usage();
+		exit(50);
+	};
+	if(	(l = strlen(argv[1])) <= strlen("-command")
+	&&	!strncmp(argv[1], "-command", l) ) {
+		return(2);
+	} else {
+		return(1);
+	}
+}
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * Set up non-X arguments
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+
+void set_up_arglist(argc, argv)
+int argc;
+char **argv;
+{
+	int i, siz, skip;
+	char *p;
+
+	skip = check_sep(argc, argv);
+
+	if(argc - skip > MAX_ARGS) {
+		char *buf[256];
+		sprintf(buf,
+		"too many command-line arguments (max %d)", MAX_ARGS);
+		msg("xpp", buf);
+		exit(51);
+	};
+		
+	siz = 0;
+
+	for(i = skip; i < argc; ++i) {
+		siz += strlen(argv[i]) + 1;
+	};
+
+	if((cmd_buf = XtMalloc(siz)) == NULL) {
+		msg("xpp",
+		"cannot allocate space for command-line\n");
+		exit(52);
+	};
+
+	p = cmd_buf;
+
+	for(i = skip; i < argc; ++i) {
+		strcpy(p, argv[i]);
+		arglist[i - skip] = p;
+		p += strlen(argv[i]) + 1;
+	};
+
+	arglist[i - skip] = NULL;
+
+}
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
  * main:
@@ -64,19 +148,24 @@ char **argv;
 {
 	debug = TRUE;
 
-	root = XtVaAppInitialize(&app,  "Xpp", NULL,
-		0, &argc, argv, NULL, NULL);
-/*
-	XtVaGetApplicationResources(root, &app_data,
-		resources,
-		XtNumber(resources),
-		NULL);
+	root = XtVaAppInitialize(&app,
+		"Xpp",
+		options,
+		XtNumber(options),
+		&argc,
+		argv, NULL, NULL);
 
-	strcpy(cmd_buf, app_data.command_line);
-*/
-	arglist[0] = argv[1];
+	set_up_arglist(argc, argv);
 
-	arglist[1] = NULL;
+if(debug = 1) {
+{int i;
+	for(i = 0; i < MAX_ARGS && arglist[i]; ++i) {
+		fprintf(stderr,
+		"arglist[%d] = \"%s\"\n", i, arglist[i]);
+	}
+}
+}
 
 	cmdwin();
+	exit(0);
 }
