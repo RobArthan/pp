6323453bb613390f1adeb2db4238e5c64c554872
Rob Arthan <rda@lemma-one.com>
1994-03-27 17:28:06 +0000

    reorganised. added file save. RDA

--- src/xpp/files.c
+++ src/xpp/files.c
@@ -35,63 +35,192 @@
 #include "xpp.h"
 
 
-static char *cant_stat_message = "That file does not seem to exist";
+static char *cant_stat_message =
+	 "The file \"%s\" does not seem to exist";
 
-static char *not_text_message = "That file is not a text file";
+static char *not_text_message =
+	 "The file \"%s\" is not a text file";
 
-static char *no_space_message = "Not enough memory to read the file";
+static char *no_space_message =
+	 "Not enough memory is available to read the file \"%s\"";
 
-static char *cant_open_message = "That file cannot be read";
+static char *cant_read_message =
+	 "The file \"%s\" cannot be opened for reading";
 
-static char *read_error_message = "Error reading the file";
+static char *cant_write_message =
+	 "The file \"%s\" cannot be opened for writing";
+
+static char *read_error_message =
+	 "Error reading the file \"%s\"";
+
+static char *write_error_message =
+	 "Error writing the file \"%s\"";
+
+static char *no_message_space_message =
+	 "Not enough memory is available to report a file error";
+
+
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * FILE OPERATIONS
+ * Private functions:
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * file_error_dialog: error reporting for the file operations
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+static void file_error_dialog(
+	Widget	w,
+	char	*fmt,
+	char	*fname)
+{
+	char	*msg;
+	if((msg = XtMalloc(strlen(fmt) + strlen(fname) - 1)) == NULL) {
+/* Not - 2 because need room for the null-padding */
+		ok_dialog(w, no_message_space_message);
+		return;
+	};
+	sprintf(msg, fmt, fname);
+	ok_dialog(w, msg);
+	XtFree(msg);
+}
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * get_file_contents: read the contents of a file into a buffer.
+ * The first parameter is the name of a widget to own any error
+ * message dialogues.
+ * The second parameter is a pointer to the file name.
+ * NULL is returned if anything goes wrong (and if NULL is
+ * returned, the user will have been presented with an error
+ * message dialogue).
+ * If all is well the return value is a pointer to a buffer
+ * containing the contents of the file as a null-terminated
+ * character array.
+ * The buffer should be XtFree'd when caller is done with it.
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
-/* **** **** **** **** **** **** **** **** **** **** **** ****
- * open a file and load it into a text widget given the
- * widget, the file name 
- * **** **** **** **** **** **** **** **** **** **** **** **** */
-
-void load_file(text, name)
-Widget text;
-char *name;
+static char *get_file_contents(
+	Widget	w,
+	char	*name)
 {
 	struct stat status;
 	NAT siz;
 	FILE *fp;
 	char *buf;
 
-	if(!(name && *name)) { /* NULL or empty */
-		XmTextSetString(text, "");
-		return;
-	};
 	if(stat(name, &status) != 0) {
-		ok_dialog(text, cant_stat_message);
-		return;
+		file_error_dialog(w, cant_stat_message, name);
+		return NULL;
 	};
 	if(!S_ISREG(status.st_mode)) {
-		ok_dialog(text, not_text_message);
-		return;
+		file_error_dialog(w, not_text_message, name);
+		return NULL;
 	};
 	siz = status.st_size;
 	if(!(buf = XtMalloc(siz + 1))) {
-		ok_dialog(text, no_space_message);
-		return;
+		file_error_dialog(w, no_space_message, name);
+		return NULL;
 	};
 	if((fp = fopen(name, "r")) == NULL) {
-		ok_dialog(text, cant_open_message);
+		file_error_dialog(w, cant_read_message, name);
 		XtFree(buf);
-		return;
+		return NULL;
 	};
 	if(fread(buf, sizeof(char), siz, fp) != siz) {
-		ok_dialog(text, read_error_message);
+		file_error_dialog(w, read_error_message, name);
 		XtFree(buf);
-		return;
+		fclose(fp);
+		return NULL;
 	};
+	fclose(fp);
 	buf[siz] = '\0';
-	XmTextSetString(text, buf);
+	return buf;
+}
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * store_file_contents: store a buffer into a file.
+ * The first parameter is the name of a widget to own any error
+ * message dialogues.
+ * The second parameter is a pointer to the file name.
+ * The third parameter is a pointer to the data as a null-terminated
+ * string.
+ * False is returned if anything goes wrong (and if NULL is
+ * returned, the user will have been presented with an error
+ * message dialogue).
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+
+static Boolean store_file_contents(
+	Widget	w,
+	char	*name,
+	char	*buf)
+{
+	struct stat status;
+	NAT siz;
+	FILE *fp;
+
+	if(stat(name, &status) != 0) {
+		file_error_dialog(w, cant_stat_message, name);
+		return False;
+	};
+	if(!S_ISREG(status.st_mode)) {
+		file_error_dialog(w, not_text_message, name);
+		return False;
+	};
+	siz = strlen(buf);
+	if((fp = fopen(name, "w")) == NULL) {
+		file_error_dialog(w, cant_write_message, name);
+		return False;
+	};
+	if(fwrite(buf, sizeof(char), siz, fp) != siz) {
+		file_error_dialog(w, read_error_message, name);
+		fclose(fp);
+		return False;
+	};
+	fclose(fp);
+	return True;
+}
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * open_file: open a file and load it into a text widget given the
+ * widget and the file name 
+ * Implements `open' in a file menu.
+ * If name is NULL or an empty string it just empties out the
+ * contents of the text window and returns False.
+ * E.g., this is done when opening the file named on the
+ * command line.
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+
+Boolean open_file(
+		Widget	text,
+		char	*name)
+{
+	char *buf;
+
+	if(!(name && *name)) { /* NULL or empty */
+		XmTextSetString(text, "");
+		return False;
+	};
+	if((buf = get_file_contents(text, name)) != NULL) {
+		XmTextSetString(text, buf);
+		XtFree(buf);
+		return True;
+	} else {
+		return False;
+	}
+}
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * save_file: store contents of a text widget into a named file
+ * which will presumably already exist and may be overwritten
+ * without confirmation.
+ * Implements `save' as opposed to `save as' in a file menu
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+
+Boolean save_file(
+	Widget	text,
+	char	*name)
+{
+	FILE *fp;
+	char *buf;
+	buf = XmTextGetString(text);
+	store_file_contents(text, name, buf);
 	XtFree(buf);
-	return;
 }
 
