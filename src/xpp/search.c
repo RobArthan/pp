7d46d350022c16ec607c9a1ca4f636612c79331c
Rob Arthan <rda@lemma-one.com>
1994-04-05 18:51:34 +0000

    first version with simple find/replace facility in place. RDA

--- src/xpp/search.c
+++ src/xpp/search.c
@@ -28,12 +28,9 @@
  * static data
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
-typedef struct {Widget text_w, shell_w, find_w, replace_w;} FindData;
+typedef struct {Widget text_w, shell_w, manager_w, find_w, replace_w;} FindData;
 
 static FindData find_data;
-
-static char *nothing_to_replace_message = 
-	"Nothing to be replaced has been selected";
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
  * add_find_tool: attach a find & replace tool to a text widget
@@ -43,14 +40,15 @@ Bool add_find_tool(Widget text_w)
 {
 	NAT cbdata;
 	Widget shell, rowcol, find_form, replace_form,
-		find_text, replace_text, find_btn, replace_btn;
+		find_text, replace_text, find_btn, replace_btn,
+		find_lab, replace_lab, button_form;
 
 	static void find_cb(), replace_cb();
 
 
-	if((find_data.find_w) != NULL) {
-		XtManageChild(find_data.find_w);
-		XtPopup(XtParent(find_data.find_w), XtGrabNone);
+	if((find_data.shell_w) != NULL) {
+		XtManageChild(find_data.manager_w);
+		XtPopup(find_data.shell_w, XtGrabNone);
 		return True;
 	};
 
@@ -73,13 +71,19 @@ Bool add_find_tool(Widget text_w)
 		XmNfractionBase,	10,
 		NULL);
 
-	find_btn = XtVaCreateManagedWidget("Find:",
-		xmPushButtonWidgetClass,	find_form,
+	button_form = XtVaCreateWidget("button form",
+		xmFormWidgetClass, rowcol,
+		XmNfractionBase,	10,
+		NULL);
+
+	find_lab = XtVaCreateManagedWidget("Search for:",
+		xmLabelWidgetClass,	find_form,
 		XmNtopAttachment,		XmATTACH_FORM,
 		XmNbottomAttachment,		XmATTACH_FORM,
 		XmNleftAttachment,		XmATTACH_FORM,
 		XmNrightAttachment,		XmATTACH_POSITION,
 		XmNrightPosition,		2,
+		XmNalignment,			XmALIGNMENT_BEGINNING,
 		NULL);
 
 	find_text = XtVaCreateManagedWidget("find string",
@@ -91,22 +95,41 @@ Bool add_find_tool(Widget text_w)
 		XmNcolumns,			40,
 		NULL);
 
-	replace_btn = XtVaCreateManagedWidget("Replace:",
-		xmPushButtonWidgetClass,	replace_form,
+	replace_lab = XtVaCreateManagedWidget("Replace With:",
+		xmLabelWidgetClass,	replace_form,
 		XmNtopAttachment,		XmATTACH_FORM,
 		XmNbottomAttachment,		XmATTACH_FORM,
 		XmNleftAttachment,		XmATTACH_FORM,
 		XmNrightAttachment,		XmATTACH_POSITION,
 		XmNrightPosition,		2,
+		XmNalignment,			XmALIGNMENT_BEGINNING,
 		NULL);
 
-	replace_text = XtVaCreateManagedWidget("replace string",
+	replace_text = XtVaCreateManagedWidget("replacement string",
 		xmTextFieldWidgetClass,	replace_form,
 		XmNtraversalOn,			True,
 		XmNrightAttachment,		XmATTACH_FORM,
 		XmNleftAttachment,		XmATTACH_POSITION,
 		XmNleftPosition,		2,
 		XmNcolumns,			40,
+		NULL);
+
+	find_btn = XtVaCreateManagedWidget("Find",
+		xmPushButtonWidgetClass,	button_form,
+		XmNtopAttachment,		XmATTACH_FORM,
+		XmNbottomAttachment,		XmATTACH_FORM,
+		XmNleftAttachment,		XmATTACH_FORM,
+		XmNrightAttachment,		XmATTACH_POSITION,
+		XmNrightPosition,		4,
+		NULL);
+
+	replace_btn = XtVaCreateManagedWidget("Find and Replace",
+		xmPushButtonWidgetClass,	button_form,
+		XmNtopAttachment,		XmATTACH_FORM,
+		XmNbottomAttachment,		XmATTACH_FORM,
+		XmNleftAttachment,		XmATTACH_POSITION,
+		XmNrightAttachment,		XmATTACH_FORM,
+		XmNleftPosition,		5,
 		NULL);
 
 	XtAddCallback(find_btn, XmNactivateCallback,
@@ -116,19 +139,21 @@ Bool add_find_tool(Widget text_w)
 		find_cb, (XtPointer)DO_FIND);
 
 	XtAddCallback(replace_btn, XmNactivateCallback,
-		replace_cb, (XtPointer)DO_REPLACE);
+		find_cb, (XtPointer)DO_REPLACE);
 
 	XtAddCallback(replace_text, XmNactivateCallback,
-		replace_cb, (XtPointer)DO_REPLACE);
+		find_cb, (XtPointer)DO_REPLACE);
 
 	find_data.text_w = text_w;
 	find_data.shell_w = shell;
+	find_data.manager_w = rowcol;
 	find_data.find_w = find_text;
 	find_data.replace_w = replace_text;
 
 
 	XtManageChild(find_form);
 	XtManageChild(replace_form);
+	XtManageChild(button_form);
 	XtManageChild(rowcol);
 	XtPopup(shell, XtGrabNone);
 
@@ -144,55 +169,46 @@ static void find_cb(
 	NAT				cbdata,
 	XmPushButtonCallbackStruct	cbs)
 {
-	int i, len;
+	int left, len;
 	NAT start_point;
 	static int find_string();
-	char *to_find, *to_search;
+	char *to_find, *to_search, *replacement;
 	to_find = XmTextGetString(find_data.find_w);
 	to_search = XmTextGetString(find_data.text_w);
 	start_point = XmTextGetInsertionPosition(find_data.text_w);
-	i = find_string(to_find, to_search, start_point);
-	if(i > 0) {
-			XmTextSetSelection(
-				find_data.text_w,
-				i,
-				i + strlen(to_find),
-				CurrentTime);
+	left = find_string(to_find, to_search, start_point);
+	if(left > 0) {
+		switch(cbdata) {
+			case DO_FIND:
+				XmTextSetSelection(
+					find_data.text_w,
+					left,
+					left + strlen(to_find),
+					CurrentTime);
+				break;
+			case DO_REPLACE:
+				replacement = XmTextGetString(
+						find_data.replace_w);
+				XmTextReplace(
+					find_data.text_w,
+ 					left,
+					left + strlen(to_find),
+					replacement);
+				XmTextSetSelection(
+					find_data.text_w,
+					left,
+					left + strlen(replacement),
+					CurrentTime);
+				XtFree(replacement);
+				break;
+			default:
+				break;
+		}
 	}
 		
 	XtFree(to_find);
 	XtFree(to_search);
 
-}
-
-/* **** **** **** **** **** **** **** **** **** **** **** ****
- * replace callback.
- * **** **** **** **** **** **** **** **** **** **** **** **** */
-static void replace_cb(
-	Widget				w,
-	NAT				cbdata,
-	XmPushButtonCallbackStruct	cbs)
-{
-	char	*replacement;
-	XmTextPosition left, right;
-
-	replacement = XmTextGetString(find_data.replace_w);
-	if(XmTextGetSelectionPosition(find_data.text_w, &left, &right)) {
-		XmTextReplace(
-			find_data.text_w,
- 			left,
-			right,
-			replacement);
-		XmTextSetSelection(
-				find_data.text_w,
-				left,
-				left + strlen(replacement),
-				CurrentTime);
-	} else {
-		ok_dialog(find_data.replace_w,
-			nothing_to_replace_message);
-	}
-	XtFree(replacement);
 }
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
