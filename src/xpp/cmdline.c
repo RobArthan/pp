21248749025b4229c2ad6480962f0b29f38f46dc
Rob Arthan <rda@lemma-one.com>
1994-08-12 17:16:00 +0000

    major redesign. RDA

--- src/xpp/cmdline.c
+++ src/xpp/cmdline.c
@@ -25,9 +25,15 @@
  * static data 
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
-typedef struct{Widget shell_w, manager_w, cmd_w;} CmdLineData;
+typedef struct{Widget shell_w, manager_w, list_w, cmd_w;} CmdLineData;
 
 static CmdLineData cmd_line_data;
+
+char *no_command_to_add_msg =
+"There is no text in the command line to add to the list.";
+
+char *nothing_to_delete_msg =
+"No item in the list has been selected.";
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
  * add_cmd_line: attach a tool allowing entry of single line
@@ -35,13 +41,21 @@ static CmdLineData cmd_line_data;
  *
  * The tool is a popup shell looking something like:
  * -------------------------------
- * | Command:   | command string |
+ * |  remembered command 1              |
+ * |  remembered command 2              |
+ * |  ...                               |
+ * |  command string                    |
+ * |  Execute Add Dismiss Delete Help   |
  * -------------------------------
  * `command string' is a text widget in which the user can
  * prepare a line of text for execution.
- * `Command' is a push button which will cause the command
+ * The `remembered commands' are in a scrolled list widget.
+ * Selecting a remembered command causes it to be entered into
+ * the command string.
+ * `Execute' is a push button which will cause the command
  * string to be executed (with a new-line tagged on the end).
- *
+ * `Add' is a push-button which will cause the contents of the command
+ * string to be added to the list.
  * The parameter is a widget to be the parent of the
  * popup shell. It should be a text widget from which the translations
  * and font for the text widget can be borrowed.
@@ -58,12 +72,20 @@ static CmdLineData cmd_line_data;
 
 void add_cmd_line(Widget text_w)
 {
-	Widget shell, cmd_btn, cmd_form, cmd_text;
+	Widget shell, paned, list_w, cmd_text,
+		btn_form, exec_btn, add_btn, delete_btn, dismiss_btn, help_btn;
+	Arg args[12];
+	int i;
 
 	XmFontList fontlist;
 
 	static void
-		cmd_line_cb();
+		list_select_cb(),
+		exec_cb(),
+		add_cb(),
+		delete_cb(),
+		dismiss_cb(),
+		help_cb();
 
 
 	if((cmd_line_data.shell_w) != NULL) {
@@ -77,31 +99,90 @@ void add_cmd_line(Widget text_w)
 		XmNdeleteResponse,		XmUNMAP,
 		NULL); 
 
-	cmd_form = XtVaCreateWidget("command_line_form",
-		xmFormWidgetClass, 		shell,
-		XmNautoUnmanage,			False,
-		XmNfractionBase,		12,
+	paned = XtVaCreateWidget("command-paned",
+		xmPanedWindowWidgetClass, 	shell,
 		NULL);
 
-	cmd_btn = XtVaCreateManagedWidget("Command:",
-		xmPushButtonWidgetClass,		cmd_form,
+
+	i = 0;
+	XtSetArg(args[i], XmNautomaticSelection,		False); ++i;
+	XtSetArg(args[i], XmNscrollBarDisplayPolicy,	XmSTATIC); ++i;
+	XtSetArg(args[i], XmNselectionPolicy,		XmBROWSE_SELECT); ++i;
+	XtSetArg(args[i], XmNlistSizePolicy,		XmCONSTANT); ++i;
+	XtSetArg(args[i], XmNvisibleItemCount,		4); ++i;
+
+	list_w = XmCreateScrolledList(paned, "command-list", args, i);
+
+{
+XmString s;
+	XmListAddItem(list_w, s = XmStringCreateSimple("undo 1;"), 0);
+	XtFree(s);
+	XmListAddItem(list_w, s = XmStringCreateSimple("print_status();"), 0);
+	XtFree(s);
+	XmListAddItem(list_w, s = XmStringCreateSimple("print_current_goal();"), 0);
+	XtFree(s);
+}
+	cmd_text = XtVaCreateManagedWidget("cmd_text",
+		xmTextWidgetClass,		paned,
+		XmNeditMode,			XmMULTI_LINE_EDIT,
+		XmNrows,			1,
+		XmNcolumns,			40,
+		NULL);
+
+
+	btn_form = XtVaCreateWidget("command_line_form",
+		xmFormWidgetClass, 		paned,
+		XmNautoUnmanage,		False,
+		XmNfractionBase,		15,
+		NULL);
+
+	exec_btn = XtVaCreateManagedWidget("Execute",
+		xmPushButtonWidgetClass,	btn_form,
 		XmNtopAttachment,		XmATTACH_FORM,
 		XmNbottomAttachment,		XmATTACH_FORM,
 		XmNleftAttachment,		XmATTACH_FORM,
 		XmNrightAttachment,		XmATTACH_POSITION,
-		XmNrightPosition,		2,
-		XmNalignment,			XmALIGNMENT_BEGINNING,
+		XmNrightPosition,		3,
 		NULL);
 
-	cmd_text = XtVaCreateManagedWidget("cmd_text",
-		xmTextWidgetClass,		cmd_form,
-		XmNeditMode,			XmMULTI_LINE_EDIT,
-		XmNrows,			1,
-		XmNcolumns,			40,
+
+	add_btn = XtVaCreateManagedWidget("Add",
+		xmPushButtonWidgetClass,	btn_form,
+		XmNtopAttachment,		XmATTACH_FORM,
+		XmNbottomAttachment,		XmATTACH_FORM,
+		XmNleftAttachment,		XmATTACH_POSITION,
+		XmNrightAttachment,		XmATTACH_POSITION,
+		XmNleftPosition,		3,
+		XmNrightPosition,		6,
+		NULL);
+
+	delete_btn = XtVaCreateManagedWidget("Delete",
+		xmPushButtonWidgetClass,	btn_form,
+		XmNtopAttachment,		XmATTACH_FORM,
+		XmNbottomAttachment,		XmATTACH_FORM,
+		XmNleftAttachment,		XmATTACH_POSITION,
+		XmNrightAttachment,		XmATTACH_POSITION,
+		XmNleftPosition,		6,
+		XmNrightPosition,		9,
+		NULL);
+
+	dismiss_btn = XtVaCreateManagedWidget("Dismiss",
+		xmPushButtonWidgetClass,	btn_form,
+		XmNtopAttachment,		XmATTACH_FORM,
+		XmNbottomAttachment,		XmATTACH_FORM,
+		XmNleftAttachment,		XmATTACH_POSITION,
+		XmNrightAttachment,		XmATTACH_POSITION,
+		XmNleftPosition,		9,
+		XmNrightPosition,		12,
+		NULL);
+
+	help_btn = XtVaCreateManagedWidget("Help",
+		xmPushButtonWidgetClass,	btn_form,
+		XmNtopAttachment,		XmATTACH_FORM,
+		XmNbottomAttachment,		XmATTACH_FORM,
 		XmNleftAttachment,		XmATTACH_POSITION,
 		XmNrightAttachment,		XmATTACH_FORM,
-		XmNleftPosition,		2,
-		XmNcolumns,			40,
+		XmNleftPosition,		12,
 		NULL);
 
 
@@ -111,7 +192,8 @@ void add_cmd_line(Widget text_w)
 
 	cmd_line_data.cmd_w = cmd_text;
 	cmd_line_data.shell_w = shell;
-	cmd_line_data.manager_w = cmd_form;
+	cmd_line_data.list_w = list_w;
+	cmd_line_data.manager_w = paned;
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
  * set up text widget translations and font
@@ -120,8 +202,11 @@ void add_cmd_line(Widget text_w)
 	XtOverrideTranslations(cmd_text, text_translations);
 
 	XtVaGetValues(text_w, XmNfontList, &fontlist, NULL);
+
+
 	if(fontlist != NULL) {
 		XtVaSetValues(cmd_text, XmNfontList, fontlist, NULL);
+		XtVaSetValues(list_w, XmNfontList, fontlist, NULL);
 	}
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
@@ -134,29 +219,49 @@ void add_cmd_line(Widget text_w)
  * add callbacks.
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
-	XtAddCallback(cmd_btn, XmNactivateCallback,
-		cmd_line_cb, (XtPointer)(&cmd_line_data));
+	XtAddCallback(exec_btn, XmNactivateCallback,
+		exec_cb, (XtPointer)(&cmd_line_data));
 
 	XtAddCallback(cmd_text, XmNactivateCallback,
-		cmd_line_cb, (XtPointer)(&cmd_line_data));
+		exec_cb, (XtPointer)(&cmd_line_data));
+
+	XtAddCallback(add_btn, XmNactivateCallback,
+		add_cb, (XtPointer)(&cmd_line_data));
+
+	XtAddCallback(delete_btn, XmNactivateCallback,
+		delete_cb, (XtPointer)(&cmd_line_data));
+
+	XtAddCallback(dismiss_btn, XmNactivateCallback,
+		dismiss_cb, (XtPointer)(&cmd_line_data));
+
+	XtAddCallback(help_btn, XmNactivateCallback,
+		help_cb, (XtPointer)(&cmd_line_data));
+
+	XtAddCallback(list_w, XmNdefaultActionCallback,
+		list_select_cb, (XtPointer)(&cmd_line_data));
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
  * Manage everything:
  * **** **** **** **** **** **** **** **** **** **** **** **** */
 
-	XtManageChild(cmd_form);
+	XtManageChild(btn_form);
+	XtManageChild(paned);
+	XtManageChild(list_w);
 	XtPopup(shell, XtGrabNone);
+	fix_pane_height(XtParent(list_w), XtParent(list_w));
+	fix_pane_height(cmd_text, cmd_text);
+	fix_pane_height(btn_form, btn_form);
 
 }
 
 
 /* **** **** **** **** **** **** **** **** **** **** **** ****
- * the callback.
+ * the callback for the execute button.
  * selects the command string after executing it to make it
  * easier to overtype the next command if the user is doing
  * many one line commands in a hurry.
  * **** **** **** **** **** **** **** **** **** **** **** **** */
-static void cmd_line_cb(
+static void exec_cb(
 	Widget				w,
 	CmdLineData			*cbdata,
 	XmPushButtonCallbackStruct	cbs)
@@ -174,6 +279,85 @@ static void cmd_line_cb(
 			CurrentTime);
 	}
 }
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * the callback for selection in the list.
+ * replaces the command line with the selected item.
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+static void list_select_cb(
+	Widget				w,
+	CmdLineData			*cbdata,
+	XmListCallbackStruct		*cbs)
+{
+	char *cmd = NULL;
+	XmStringGetLtoR(cbs->item, XmSTRING_DEFAULT_CHARSET, &cmd);
+	if(cmd) {
+		XmTextSetString(cbdata->cmd_w, cmd);
+		XtFree(cmd);
+	} 
+}
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * the callback for the add button.
+ * adds the contents of the command line to the list.
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+static void add_cb(
+	Widget				w,
+	CmdLineData			*cbdata,
+	XmPushButtonCallbackStruct	cbs)
+{
+	char *cmd;
+	XmString item;
+	if((cmd = XmTextGetString(cbdata->cmd_w)) != NULL && *cmd) {
+		item = XmStringCreateSimple(cmd);
+		XmListAddItem(cbdata->list_w, item, 0);
+		XmListSelectPos(cbdata->list_w, 0, False);
+		XmListSetBottomPos(cbdata->list_w, 0);
+		XmStringFree(item);
+	} else {
+		ok_dialog(root, no_command_to_add_msg);
+	}
+}
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * the callback for the delete button.
+ * deletes the selected item.
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+static void delete_cb(
+	Widget				w,
+	CmdLineData			*cbdata,
+	XmAnyCallbackStruct		*cbs)
+{
+	int *posns;
+	int num_posns;
+	if(	XmListGetSelectedPos(cbdata->list_w, &posns, &num_posns)
+	&&	num_posns) {
+		XmListDeletePos(cbdata->list_w, *posns);
+		XtFree((char *)posns);
+	} else {
+		ok_dialog(root, nothing_to_delete_msg);
+	}
+}
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * dismiss callback.
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+static void dismiss_cb(
+	Widget				w,
+	CmdLineData			*cbdata,
+	XmPushButtonCallbackStruct	cbs)
+{
+	XtPopdown(cbdata->shell_w);
+}
 
 
+
+/* **** **** **** **** **** **** **** **** **** **** **** ****
+ * help callback.
+ * **** **** **** **** **** **** **** **** **** **** **** **** */
+static void help_cb(
+	Widget				w,
+	CmdLineData			*cbdata,
+	XmPushButtonCallbackStruct	cbs)
+{
+	help_dialog(cbdata->shell_w, Help_Command_Line_Tool);
+}
 
