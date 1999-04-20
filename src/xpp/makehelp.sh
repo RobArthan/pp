707cc07b507a2f81f42241b3a23167359a2b52a6
Xpp User <rda@lemma-one.com>
1999-04-20 12:07:32 +0000

    converted to use ed rather than ex for portability.

--- src/xpp/makehelp.sh
+++ src/xpp/makehelp.sh
@@ -1,5 +1,5 @@
 #
-# $Id$
+# $Id: makehelp.sh,v 2.1 1994/08/30 13:37:40 djk Exp xpp $
 # makehelp - ProofPower X/Motif Interface build tool
 # This shell script constructs the files and help.c and help.h from help.txt
 # using nroff -ms to do the formatting.
@@ -10,50 +10,52 @@
 # const String Help_A_Help_Title =
 # "==== A Help Title ===="
 # ...
-# It replaces sequences of > 2 new lines by sequences of 2
+# It replaces multiple blank lines by single blank lines
+# It assumes that the input does not contain lines containing only
+# hash signs and that lines beginning with asterisks are the
+# message headers to be turned into C string declarations.
+#
 # 
 rm -f help.c
 rm -f help.h
-ex >/dev/null 2>&1 -s <<\!
+# ex >/dev/null 2>&1 -s <<\!
+ed <<\!
 r ! nroff -ms help.txt 
-1,/^\*/-1d
-g/^$/j|s/./\
-&/
-g/^$/j|s/./\
-&/
-g/^$/j|s/./\
-&/
-g/^$/j|s/./\
-&/
-g/^$/j|s/./\
-&/
-g/^$/j|s/./\
-&/
-g/^$/j|s/./\
-&/
-g/^$/j|s/./\
-&/
-g/^$/j|s/./\
-&/
+g/^#*$/s/^/#/
+1,$-1g/^#*$/j
+1,$-1g/^#*$/j
+1,$-1g/^#*$/j
+1,$-1g/^#*$/j
+1,$-1g/^#*$/j
+1,$-1g/^#*$/j
+1,$-1g/^#*$/j
+1,$-1g/^#*$/j
+g/^##*/s/#*/\
+/
 %s/ *$//
-g/^\*/s/^* *//|s/.*/* &\
+g/^\*/s/.*/&\
 ==== & ====/
-v/^*/s/^/"/|s/$/\\n"/
-g/^\*/s/^\* *//|s/ /_/g|s/^/;\
-const String Help_/|s/$/ =/
+v/^\*/s/^/"/
+v/^\*/s/$/\\n"/
+g/^\*/s/ /_/g
+g/^\*/s/$/ =/
+g/^\*/s/^/;\
+const String Help_/
 1d
 $s/$/;/
+g/^"==== \*/s/ \*//
+g/^const/s/\*_//1
 0a
 /*
  * Help texts for the ProofPower X/Motif Interface.
  * This file is automatically created from help.txt.
  * Do not edit.
- * Made by $RCSfile$ version $Revision$ of $Date$
+ * Made by $RCSfile: makehelp.sh,v $ version $Revision: 2.1 $ of $Date: 1994/08/30 13:37:40 $
  */
 #define _help
 #include "xpp.h"
 .
-w! help.c
+w help.c
 v/^const String Help_/d
 %s/^/extern /
 %s/ =/;/
@@ -62,9 +64,9 @@ v/^const String Help_/d
  * Help include file for the ProofPower X/Motif Interface.
  * This file is automatically created from help.txt.
  * Do not edit.
- * Made by $RCSfile$ version $Revision$ of $Date$
+ * Made by $RCSfile: makehelp.sh,v $ version $Revision: 2.1 $ of $Date: 1994/08/30 13:37:40 $
  */
 .
-w! help.h
+w help.h
 q
 !
