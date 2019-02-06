7cdddf9008f37dea73f297d8a55b8ea76a2ad9d0
Rob Arthan <rda@lemma-one.com>
2019-02-06 13:03:22 +0000

    Now uses awk and UnicodeData.txt to give information about control glyphs etc.

--- src/xpp/ucdnames.sh
+++ src/xpp/ucdnames.sh
@@ -1,27 +1,42 @@
 #
-# ucdnames.sh generates ucdnames.h from the NamesList.txt supplied with
-# the Unicode Character Database:
+# ucdnames.sh generates ucdnames.h from the the Unicode Character Database:
 # http://www.unicode.org/Public/zipped/11.0.0/
+# Uses an awk script to extract a list of codepoint/name pairs from
+# from the file UnicodeData.txt
+#
+# Rob Arthan rda@lemma-one.com
+# 4th February 2019
 #
 cat > ucdnames.h <<++++
 /* **** **** **** **** **** **** **** **** **** **** **** **** ****
- * This file is generated from the file NamesList.txt supplied with
- * the Unicode Character Database
- *
- * Rob Arthan rda@lemma-one.com
- * 4th February 2019
+ * This file is generated from the Unicode Character Database:
+ * http://www.unicode.org/Public/zipped/11.0.0/
  **** **** **** **** **** **** **** **** **** **** **** **** **** */
 typedef struct {
-	wchar_t	code_point;
+	wchar_t	codepoint;
 	char	*name;
 } ucd_name;
 static ucd_name ucd_names[]  = {
 ++++
-sed -n -e '/^[0-9A-Faa-f]/p' <NamesList.txt | \
-sed -e '{
-	1,$s/\(.*\)	\(.*\)/	{0x\1,	"\2"},/
-	$s/,$//
-}' >> ucdnames.h
+awk -F ';' < UnicodeData.txt '
+	BEGIN {
+		started = 0
+	}
+	{
+		if (started) {
+			printf ",\n"
+		}
+		started = 1
+		if( substr($2, 1, 1) != "<" || $11 == "" ) {
+			printf "\t{0x%s,\t\"%s\"}", $1, $2
+		} else {
+			printf "\t{0x%s,\t\"%s %s\"}", $1, $2, $11
+		}
+	}
+	END	{
+		printf "\n"
+	}
+' >> ucdnames.h
 cat >> ucdnames.h <<++++
 };
 ++++
