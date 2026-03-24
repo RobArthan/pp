eb89eab06def94625fdd1557a71b24063eb6205b
Rob Arthan <rda@Robs-MacBook-Pro.local>
2026-03-24 22:05:57 +0000

    Fixed misuse of a gadget in the Templates help dialogue

--- src/xpp/templates.c
+++ src/xpp/templates.c
@@ -446,10 +446,10 @@ static void templates_help_cb(
 		NULL);
 
 	introduction = XtVaCreateManagedWidget("introduction",
-		xmLabelGadgetClass,	help_pane,
+		xmLabelWidgetClass,	help_pane,
 		XmNlabelString,		format_msg(Help_Templates_Tool,
 						HELP_LINE_LEN),
-		XmNalignment,		XmALIGNMENT_CENTER,
+		XmNalignment,		XmALIGNMENT_BEGINNING,
 		NULL);
 
 	XtManageChild(introduction);
